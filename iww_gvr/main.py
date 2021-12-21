735857a09a10c2a4bdecd5a1009eb32e3b72dcfd
dvp <dmitri_portnov@yahoo.com>
2021-12-21 19:10:11 +0300

    fix: missed main module - neccessary to organize ordinary CLI script entry point

---
+++ iww_gvr/main.py
@@ -0,0 +1,17 @@
+"""The iww_gvr CLI.
+
+Run iww_gvr in interactive mode.
+
+"""
+
+# TODO dvp: if needed, then add here passing command line args, configuring, logging and so on.
+
+from iww_gvr.menu import Menu
+
+
+def main():
+    Menu()
+
+
+if __name__ == "__main__":
+    main()
